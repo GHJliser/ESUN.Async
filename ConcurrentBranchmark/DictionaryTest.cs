711281fde56a1666fe013f77d2e826c420f14091
青椒 <ufo3210@yahoo.com.tw>
2021-09-24 13:50:49 +0800

    Conncurrent Testing

---
+++ ConcurrentBranchmark/DictionaryTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Text;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace ConcurrentBenchmark
+{
+    public class DictionaryTest
+    {
+        [Benchmark]
+        public void Test100_Dic() => CreateDic(100);
+        [Benchmark]
+        public void Test100_Con() => CreateConDic(100);
+        [Benchmark]
+        public void Test1000_Dic() => CreateDic(1000);
+        [Benchmark]
+        public void Test1000_Con() => CreateConDic(1000);
+        [Benchmark]
+        public void Test10000_Dic() => CreateDic(10000);
+        [Benchmark]
+        public void Test10000_Con() => CreateConDic(10000);
+
+        private void CreateDic(int size)
+        {
+            var dic = new Dictionary<int, object>();
+            for (int i = 0; i < size; i++)
+            {
+                dic.Add(i, new object());
+            }
+        }
+        private void CreateConDic(int size)
+        {
+            var dic = new ConcurrentDictionary<int, object>();
+            for (int i = 0; i < size; i++)
+            {
+                dic.TryAdd(i, new object());
+            }
+        }
+    }
+}
---
+++ ConcurrentBranchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace ConcurrentBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var summary = BenchmarkRunner.Run<DictionaryTest>();
+        }
+    }
+}
